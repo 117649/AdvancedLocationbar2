d14431be2e335c5fe759a5f32bf842d893adefec
7^6 <lhw.1012@gmail.com>
2026-09-03 01:02:26 +0800

    chore: clean XPI packaging

--- AdvancedLocationbar/pack.ps1
+++ AdvancedLocationbar/pack.ps1
@@ -1,3 +1,3 @@
-Remove-Item -Path .\addon.xpi
-Get-ChildItem -Path .\ -Exclude pack.ps1,*.xpi | Compress-Archive -CompressionLevel NoCompression -DestinationPath addon
-Rename-Item -Path .\addon.zip -NewName addon.xpi
+Remove-Item -Path "$PSScriptRoot\addon.xpi" -ErrorAction Ignore
+Get-ChildItem -Path $PSScriptRoot -Exclude pack.ps1,*.xpi | Compress-Archive -CompressionLevel NoCompression -DestinationPath "$PSScriptRoot\addon"
+Rename-Item -Path "$PSScriptRoot\addon.zip" -NewName addon.xpi
